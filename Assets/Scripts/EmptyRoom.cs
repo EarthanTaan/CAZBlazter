5335c45049c8f880473024a9674c3c9abdd52851
WittlyEli <162608185+EliksonRod@users.noreply.github.com>
2026-05-04 10:56:22 -0400

    Using asset placeholders

--- Assets/Scripts/EmptyRoom.cs
+++ Assets/Scripts/EmptyRoom.cs
@@ -3,4 +3,11 @@ using UnityEngine;
 public class EmptyRoom: MonoBehaviour
 {
     //Used to target empty placeholder rooms
+
+    //Determine which doors are active
+    
+    [SerializeField] public bool OpenUp;
+    [SerializeField] public bool OpenDown;
+    [SerializeField] public bool OpenRight;
+    [SerializeField] public bool OpenLeft;
 }
