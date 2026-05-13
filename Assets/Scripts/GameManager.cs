a16c1beba32cfebf84eb8a2384761b809b8ba6b7
WittlyEli <162608185+EliksonRod@users.noreply.github.com>
2026-05-13 14:12:46 -0400

    Added main menu

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,6 +4,10 @@ public class GameManager : MonoBehaviour
 {
     //float totalValue = 0f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    //Create Instance of GameManager to be used in other scripts
+    public static GameManager Instance { get; private set; }
+
+
     void Start()
     {
         
---
+++ Assets/Scripts/Player/Journal.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class Journal : MonoBehaviour
+{
+    [SerializeField] LayoutRandomizer layoutRandomizer;
+    float HouseValue = 100f; // Base value of the house
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+       
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
